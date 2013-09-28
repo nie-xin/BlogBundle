917f5783b0682084255ed987714e321ec0eaf1ce
nie xin <niexin.study@gmail.com>
2013-09-28 23:46:03 +0200

    more about router

--- BlogBundle/Controller/BlogController.php
+++ BlogBundle/Controller/BlogController.php
@@ -14,4 +14,12 @@ class BlogController extends Controller
     public function quitAction() {
         return $this->render('BlogBundle:Blog:quit.html.twig', array('name' => 'NIE Xin'));
     }
+    
+    public function readAction($id) {
+        return new Response("Article id :" . $id . " to display.");
+    }
+    
+    public function readSlugAction($year, $slug, $format) {
+        return new Response("We can display the article: slug:" . $slug . " in ". $year . " with format " . $format);
+    }
 }
